19519b3e2833b9e3afb07d3431d0826cc830fdc3
OMKAR CHAVAN <ovchavan3456@gmail.com>
2025-06-07 10:18:20 -0700

    Add files via upload

---
+++ string.java
@@ -0,0 +1,16 @@
+package string;
+
+public class string {
+	
+	
+	    public static void main(String[] args) {
+	        // Declare and initialize a string
+	        String greeting = "Hello, omkar!";
+
+	        // Print the string
+	        System.out.println(greeting);
+	    }
+	}
+
+
+
